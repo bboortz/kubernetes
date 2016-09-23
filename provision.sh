3b58fb32937c27e1b16a08daf97836006d2038a4
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2016-09-23 22:03:19 +0200

    hot file splitted into 3 files /BB

--- provision.sh
+++ provision.sh
@@ -5,19 +5,20 @@ set -u
 
 STACK_FILE="base.yaml"
 STACK_NAME="base"
-INSTANCES=1
+INSTANCE_COUNT=1
 FLAVOR="1 GB Performance"
 
 DATE=$( date "+%s" )
 ID=$$-$RANDOM
 TAGS="${STACK_NAME},${DATE},${ID}"
 
-WAIT_DELAY=15
+WAIT_DELAY=5
 
-./venv/bin/openstack stack create -f table --parameter "stack_name=${STACK_NAME};instances=${INSTANCES};flavor=${FLAVOR}" --tags "${TAGS}" --template ${STACK_FILE} ${STACK_NAME}
+./venv/bin/openstack stack create -f table --parameter "stack_name=${STACK_NAME};instance_count=${INSTANCE_COUNT};flavor=${FLAVOR}" --tags "${TAGS}" --template ${STACK_FILE} ${STACK_NAME}
 
 while true; do
-	./venv/bin/openstack stack list --tags "${TAGS}" | grep "CREATE_IN_PROGRESS" || break
+	./venv/bin/openstack stack list --tags "${TAGS}" | grep "CREATE_IN_PROGRESS" > /dev/null || break
+	echo -n "."
 	sleep ${WAIT_DELAY}
 done
 
