04f258ac36c231e652b58ee4c772ac9093b2bcd7
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2016-09-25 09:08:52 +0200

    list.sh added /BB

---
+++ list.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -i
+set -u
+
+STACK_FILE="base.yaml"
+STACK_NAME="base"
+INSTANCE_COUNT=1
+FLAVOR="1 GB Performance"
+
+DATE=$( date "+%s" )
+ID=$$-$RANDOM
+TAGS="${STACK_NAME},${DATE},${ID}"
+
+DEBUG="--debug"
+DEBUG=""
+WAIT_DELAY=5
+
+
+./venv/bin/openstack stack list -f value | awk '{ print $2 }' | while read f; do
+	./venv/bin/openstack stack resource list $f
+done
+
